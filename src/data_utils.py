2f5b07633c84c301ad11957287bd7a4e223f140e
Zacchaeus14 <yw3642@nyu.edu>
2022-06-28 08:57:22 +0800

    add mp

--- src/data_utils.py
+++ src/data_utils.py
@@ -1,11 +1,13 @@
 import os
 
+import numpy as np
+from joblib import Parallel, delayed
 from tqdm import tqdm
 
 LABEL_MAPPING = {"Ineffective": 0, "Adequate": 1, "Effective": 2}
 
 
-def prepare_training_data(indir, tokenizer, df, max_len):
+def prepare_data(indir, tokenizer, df, max_len):
     training_samples = []
     for _, row in tqdm(df.iterrows(), total=len(df)):
         idx = row["essay_id"]
@@ -35,4 +37,18 @@ def prepare_training_data(indir, tokenizer, df, max_len):
             sample["label"] = LABEL_MAPPING[label]
 
         training_samples.append(sample)
+    return training_samples
+
+
+def prepare_data_mp(indir, tokenizer, df, max_len, j=8):
+    training_samples = []
+
+    df_splits = np.array_split(df, j)
+
+    results = Parallel(n_jobs=j, backend="multiprocessing")(
+        delayed(prepare_data)(indir, tokenizer, df, max_len) for df in df_splits
+    )
+    for result in results:
+        training_samples.extend(result)
+
     return training_samples
