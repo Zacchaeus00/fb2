f1ff69382a6d340cbe964e8b70ee67bb2e89e3b7
Zacchaeus14 <yw3642@nyu.edu>
2022-06-30 06:10:24 +0800

    add files

--- src/model_utils.py
+++ src/model_utils.py
@@ -1,16 +1,26 @@
 import torch
+from transformers.trainer_pt_utils import get_parameter_names
+
+
+def get_deberta_v2_layers(model, n):
+    for layer in model.deberta.encoder.layer[-n:]:
+        yield layer
 
 
 # Re-init script copied from https://github.com/asappresearch/revisit-bert-finetuning/blob/master/run_glue.py
-def reinit_deberta_v2_pooler(model):
+def reinit_pooler(model):
     model.pooler.dense.weight.data.normal_(mean=0.0, std=model.config.initializer_range)
     model.pooler.dense.bias.data.zero_()
     for p in model.pooler.parameters():
         p.requires_grad = True
 
 
-def reinit_deberta_v2_layers(model, n=0):
-    for layer in model.deberta.encoder.layer[-n:]:
+def reinit_layers(model, n=0):
+    if model.config.model_type == "deberta-v2":
+        layer_generator = get_deberta_v2_layers(model, n)
+    else:
+        raise NotImplementedError
+    for layer in layer_generator:
         for module in layer.modules():
             if isinstance(module, (torch.nn.Linear, torch.nn.Embedding)):
                 module.weight.data.normal_(mean=0.0, std=model.config.initializer_range)
@@ -19,3 +29,8 @@ def reinit_deberta_v2_layers(model, n=0):
                 module.weight.data.fill_(1.0)
             if isinstance(module, torch.nn.Linear) and module.bias is not None:
                 module.bias.data.zero_()
+
+
+# https://github.com/huggingface/transformers/blob/v4.20.1/src/transformers/trainer.py#L209
+def get_optimizer_grouped_parameters(model, n=0):
+    raise NotImplementedError
