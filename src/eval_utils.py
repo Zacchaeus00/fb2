8c6f6ef9a5b808c8b1ee6e8748d56c6b94cf9995
Zacchaeus14 <yw3642@nyu.edu>
2022-06-30 12:49:24 +0800

    infer no grad

--- src/eval_utils.py
+++ src/eval_utils.py
@@ -7,11 +7,13 @@ from tqdm import tqdm
 
 def eval_token_cls_model(model, samples, device="cuda"):
     model = model.to(device)
+    model.eval()
     predictions = []
     labels = []
     for sample in tqdm(samples):
         input = torch.tensor(sample['input_ids']).unsqueeze(0).to(device)
-        logits = model(input).logits.squeeze()
+        with torch.no_grad():
+            logits = model(input).logits.squeeze()
         label_idxs = torch.tensor(sample['label_positions'])
         prediction = logits[label_idxs].cpu().detach().numpy()
         predictions.append(prediction)
