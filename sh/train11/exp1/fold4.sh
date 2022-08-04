59e887421c9ce83a41b5df9a130fa5fc97997eae
Zacchaeus14 <yw3642@nyu.edu>
2022-08-05 03:11:30 +0800

    update exps

--- sh/train11/exp1/fold4.sh
+++ sh/train11/exp1/fold4.sh
@@ -11,7 +11,7 @@
 #SBATCH --error=log/%x-%A.err
 #SBATCH --gres=gpu:1
 #SBATCH -p aquila
-#SBATCH --nodelist=agpu8
+#SBATCH --nodelist=agpu7
 
 module purge
 module load anaconda3 cuda/11.1.1
@@ -26,5 +26,5 @@ source /gpfsnyu/packages/anaconda3/5.2.0/bin/activate kaggle
 python -u train11.py --ckpt /gpfsnyu/scratch/yw3642/hf-models/microsoft_deberta-xlarge \
 --epochs 10 --batch_size 2 --lr 5e-6 --lr_head 1e-4 --seed -1 --fold 4 --exp 1 \
 --use_pretrained /gpfsnyu/scratch/yw3642/fb2/ckpt/pretrain0/exp19/tmp/checkpoint-272895/ \
---adv_lr 1 --adv_eps 0.0002 --adv_after_epoch 1 --patience 30 --gradient_checkpointing --warmup_ratio 0.1
+--adv_lr 1 --adv_eps 0.00015 --adv_after_epoch 1 --patience 30 --gradient_checkpointing --warmup_ratio 0.1
 echo "FINISH"
