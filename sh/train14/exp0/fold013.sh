8ec4104f91054c7aa4c3e463a26ba18a7c2c4999
Zacchaeus14 <yw3642@nyu.edu>
2022-08-10 04:42:02 +0800

    add fold 13 exp

--- sh/train14/exp0/fold013.sh
+++ sh/train14/exp0/fold013.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=train14-exp0-fold013
+#SBATCH --job-name=train14-exp0-fold13
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
@@ -23,11 +23,6 @@ cd /gpfsnyu/scratch/yw3642/fb2/src
 echo "START"
 source deactivate
 source /gpfsnyu/packages/anaconda3/5.2.0/bin/activate kaggle
-python -u train14.py --ckpt /gpfsnyu/scratch/yw3642/hf-models/microsoft_deberta-v2-xlarge \
---epochs 15 --batch_size 2 --lr 2.5e-6 --lr_head 1e-4 --seed -1 --fold 0 --exp 0 \
---use_pretrained /gpfsnyu/scratch/yw3642/fb2/ckpt/pretrain0/exp24/microsoft_deberta-v2-xlarge/checkpoint-389850/ \
---adv_lr 1 --adv_eps 0.0001 --adv_after_epoch 1.5 --patience 30 --gradient_checkpointing --warmup_ratio 0.1
-
 python -u train14.py --ckpt /gpfsnyu/scratch/yw3642/hf-models/microsoft_deberta-v2-xlarge \
 --epochs 15 --batch_size 2 --lr 2.5e-6 --lr_head 1e-4 --seed -1 --fold 1 --exp 0 \
 --use_pretrained /gpfsnyu/scratch/yw3642/fb2/ckpt/pretrain0/exp24/microsoft_deberta-v2-xlarge/checkpoint-389850/ \
