e9172419d979335f6980222d9cd5e3796b01c131
Zacchaeus14 <yw3642@nyu.edu>
2022-08-14 18:17:11 +0800

    add train16

---
+++ src/nbroad.py
@@ -0,0 +1,221 @@
+import codecs
+import os
+import re
+from functools import partial
+from pathlib import Path
+from typing import Tuple
+
+import pandas as pd
+from datasets import Dataset
+from text_unidecode import unidecode
+from transformers import AutoTokenizer
+
+
+# https://www.kaggle.com/competitions/feedback-prize-2021/discussion/313330
+def replace_encoding_with_utf8(error: UnicodeError) -> Tuple[bytes, int]:
+    return error.object[error.start : error.end].encode("utf-8"), error.end
+
+def replace_decoding_with_cp1252(error: UnicodeError) -> Tuple[str, int]:
+    return error.object[error.start : error.end].decode("cp1252"), error.end
+
+def resolve_encodings_and_normalize(text: str) -> str:
+    """Resolve the encoding problems and normalize the abnormal characters."""
+    text = (
+        text.encode("raw_unicode_escape")
+        .decode("utf-8", errors="replace_decoding_with_cp1252")
+        .encode("cp1252", errors="replace_encoding_with_utf8")
+        .decode("utf-8", errors="replace_decoding_with_cp1252")
+    )
+    text = unidecode(text)
+    return text
+
+
+def read_text_files(example, data_dir):
+    id_ = example["essay_id"]
+    with open(data_dir / "train" / f"{id_}.txt", "r") as fp:
+        example["text"] = resolve_encodings_and_normalize(fp.read())
+    return example
+
+def get_dataset(cfg):
+    # Register the encoding and decoding error handlers for `utf-8` and `cp1252`.
+    codecs.register_error("replace_encoding_with_utf8", replace_encoding_with_utf8)
+    codecs.register_error("replace_decoding_with_cp1252", replace_decoding_with_cp1252)
+
+    data_dir = Path(cfg["data_dir"])
+    train_df = pd.read_csv(data_dir / "train.csv")
+
+    text_ds = Dataset.from_dict({"essay_id": train_df.essay_id.unique()})
+
+    text_ds = text_ds.map(
+        partial(read_text_files, data_dir=data_dir),
+        num_proc=cfg["num_proc"],
+        batched=False,
+        desc="Loading text files",
+    )
+
+    text_df = text_ds.to_pandas()
+
+    train_df["discourse_text"] = [
+        resolve_encodings_and_normalize(x) for x in train_df["discourse_text"]
+    ]
+    train_df = train_df.merge(text_df, on="essay_id", how="left")
+
+    disc_types = [
+        "Claim",
+        "Concluding Statement",
+        "Counterclaim",
+        "Evidence",
+        "Lead",
+        "Position",
+        "Rebuttal",
+    ]
+    cls_tokens_map = {label: f"[CLS_{label.upper()}]" for label in disc_types}
+    end_tokens_map = {label: f"[END_{label.upper()}]" for label in disc_types}
+
+    label2id = {
+        "Adequate": 0,
+        "Effective": 1,
+        "Ineffective": 2,
+    }
+
+    tokenizer = AutoTokenizer.from_pretrained(cfg["model_name_or_path"])
+    tokenizer.add_special_tokens(
+        {"additional_special_tokens": list(cls_tokens_map.values()) + list(end_tokens_map.values())}
+    )
+    cls_id_map = {
+        label: tokenizer.encode(tkn)[1]
+        for label, tkn in cls_tokens_map.items()
+    }
+
+    def find_positions(example):
+
+        text = example["text"][0]
+
+        # keeps track of what has already
+        # been located
+        min_idx = 0
+
+        # stores start and end indexes of discourse_texts
+        idxs = []
+
+        for dt in example["discourse_text"]:
+            # calling strip is essential
+            matches = list(re.finditer(re.escape(dt.strip()), text))
+
+            # If there are multiple matches, take the first one
+            # that is past the previous discourse texts.
+            if len(matches) > 1:
+                for m in matches:
+                    if m.start() >= min_idx:
+                        break
+            # If no matches are found
+            elif len(matches) == 0:
+                idxs.append([-1])  # will filter out later
+                continue
+                # If one match is found
+            else:
+                m = matches[0]
+
+            idxs.append([m.start(), m.end()])
+
+            min_idx = m.start()
+
+        return idxs
+
+    def tokenize(example):
+        example["idxs"] = find_positions(example)
+
+        text = example["text"][0]
+        chunks = []
+        labels = []
+        prev = 0
+
+        zipped = zip(
+            example["idxs"],
+            example["discourse_type"],
+            example["discourse_effectiveness"],
+        )
+        for idxs, disc_type, disc_effect in zipped:
+            # when the discourse_text wasn't found
+            if idxs == [-1]:
+                continue
+
+            s, e = idxs
+
+            # if the start of the current discourse_text is not
+            # at the end of the previous one.
+            # (text in between discourse_texts)
+            if s != prev:
+                chunks.append(text[prev:s])
+                prev = s
+
+            # if the start of the current discourse_text is
+            # the same as the end of the previous discourse_text
+            if s == prev:
+                chunks.append(cls_tokens_map[disc_type])
+                chunks.append(text[s:e])
+                chunks.append(end_tokens_map[disc_type])
+
+            prev = e
+
+            labels.append(label2id[disc_effect])
+
+        tokenized = tokenizer(
+            " ".join(chunks),
+            padding=False,
+            truncation=False,
+            add_special_tokens=True,
+        )
+
+        # at this point, labels is not the same shape as input_ids.
+        # The following loop will add -100 so that the loss function
+        # ignores all tokens except CLS tokens
+
+        # idx for labels list
+        idx = 0
+        final_labels = []
+        for id_ in tokenized["input_ids"]:
+            # if this id belongs to a CLS token
+            if id_ in cls_id_map.values():
+                final_labels.append(labels[idx])
+                idx += 1
+            else:
+                # -100 will be ignored by loss function
+                final_labels.append(-100)
+
+        tokenized["labels"] = final_labels
+
+        return tokenized
+
+    # make lists of discourse_text, discourse_effectiveness
+    # for each essay
+    grouped = train_df.groupby(["essay_id"]).agg(list)
+
+    ds = Dataset.from_pandas(grouped)
+
+    ds = ds.map(
+        tokenize,
+        batched=False,
+        num_proc=cfg["num_proc"],
+        desc="Tokenizing",
+    )
+    return ds, tokenizer
+
+if __name__ == '__main__':
+    cfg = {
+        'num_proc': os.cpu_count(),
+        "data_dir": "../data/feedback-prize-effectiveness",
+        "model_name_or_path": "microsoft/deberta-v3-base",
+    }
+    ds, tokenizer = get_dataset(cfg)
+    print(ds)
+    # print(tokenizer.decode(ds[0]['input_ids']))
+    fold_df = pd.read_csv('../data/train_folds.csv')
+    folds = []
+    for sample in ds:
+        eid = sample['essay_id']
+        df = fold_df[fold_df['essay_id']==eid]
+        assert(df['kfold'].nunique()==1)
+        folds.append(df['kfold'].values[0])
+    ds = ds.add_column("fold", folds)
+    print(ds)
---
+++ src/train16.py
@@ -0,0 +1,123 @@
+# nbroad + Model8
+import argparse
+import datetime
+import os
+from pathlib import Path
+
+import numpy as np
+import torch
+from tez import Tez, TezConfig
+from tez.callbacks import EarlyStopping
+from torch.utils.checkpoint import checkpoint
+from transformers import DataCollatorForTokenClassification
+
+from model_utils import Model8, strip_state_dict
+from nbroad import get_dataset
+from utils import seed_everything, check_gpu
+
+os.environ['TOKENIZERS_PARALLELISM'] = 'false'
+
+
+def parse_args_train():
+    parser = argparse.ArgumentParser(description='')
+    arg = parser.add_argument
+    arg('--ckpt', type=str, default='microsoft/deberta-v3-base')
+    arg('--epochs', type=int, default=10)
+    arg('--batch_size', type=int, default=2)
+    arg('--lr', type=float, default=2e-5)
+    arg('--lr_head', type=float, default=1e-4)
+    arg('--seed', type=int, default=42)
+    arg('--fold', default=0, type=int)
+    arg('--exp', required=True, type=str)
+    arg('--gradient_checkpointing', action="store_true", required=False)
+    arg('--use_pretrained', type=str, default='')
+    arg('--adv_lr', type=float, default=0)
+    arg('--adv_eps', type=float, default=0.01)
+    arg('--adv_after_epoch', type=float, default=0)
+    arg('--only_infer', action="store_true", required=False)
+    arg('--clip_grad_norm', type=float, default=10)
+    arg('--patience', type=int, default=10)
+    arg('--pooling', type=str, default='cls')
+    arg('--reduction', type=str, default='mean')
+    arg('--warmup_ratio', type=float, default=0)
+    arg('--fix', action="store_true", required=False)
+    return parser.parse_args()
+
+
+check_gpu()
+cfg = parse_args_train()
+print(datetime.datetime.now())
+print(cfg)
+seed = cfg.seed if cfg.seed != -1 else np.random.randint(0, 10000)
+print("seed=", seed)
+seed_everything(seed)
+nb_cfg = {
+    'num_proc': os.cpu_count(),
+    "data_dir": "../data/feedback-prize-effectiveness",
+    "model_name_or_path": cfg.ckpt,
+}
+ds, tokenizer = get_dataset(nb_cfg)
+print("dataset:", ds)
+print("dataset[0]:", ds[0])
+keep_cols = {"input_ids", "attention_mask", "labels"}
+train_idxs = [i for i, sample in enumerate(ds) if sample['fold'] != cfg.fold]
+eval_idxs = [i for i, sample in enumerate(ds) if sample['fold'] == cfg.fold]
+train_dataset = ds.select(train_idxs).remove_columns([c for c in ds.column_names if c not in keep_cols])
+eval_dataset = ds.select(eval_idxs).remove_columns([c for c in ds.column_names if c not in keep_cols])
+print(f"fold {cfg.fold}: n_train={len(train_dataset)}, n_val={len(eval_dataset)}")
+output_dir = f"../ckpt/{os.path.basename(__file__).split('.')[0]}/exp{cfg.exp}/"
+Path(output_dir).mkdir(parents=True, exist_ok=True)
+
+ckpt = cfg.use_pretrained if os.path.isdir(cfg.use_pretrained) else cfg.ckpt
+print("ckpt:", ckpt)
+model = Model8(ckpt,
+               num_train_steps=int(len(train_dataset) / cfg.batch_size * cfg.epochs),
+               lr=cfg.lr,
+               lr_head=cfg.lr_head,
+               reduction=cfg.reduction,
+               warmup_ratio=cfg.warmup_ratio
+               )
+if cfg.use_pretrained and not os.path.isdir(cfg.use_pretrained):
+    model.backbone.load_state_dict(strip_state_dict(torch.load(cfg.use_pretrained), cfg.ckpt), strict=False)
+if cfg.gradient_checkpointing:
+    model.backbone.gradient_checkpointing_enable()
+model = Tez(model)
+if not cfg.only_infer:
+    es = EarlyStopping(
+        monitor="valid_loss",
+        model_path=os.path.join(output_dir, f"fold{cfg.fold}.pt"),
+        patience=cfg.patience,
+        mode="min",
+        delta=0.001,
+        save_weights_only=True,
+    )
+    config = TezConfig(
+        training_batch_size=cfg.batch_size,
+        validation_batch_size=cfg.batch_size,
+        epochs=cfg.epochs,
+        fp16=True,
+        step_scheduler_after="batch",
+        val_strategy="batch",
+        val_steps=500,
+        adv_lr=cfg.adv_lr,
+        adv_eps=cfg.adv_eps,
+        adv_after_epoch=cfg.adv_after_epoch,
+        valid_shuffle=False,
+        clip_grad_norm=cfg.clip_grad_norm,
+    )
+    model.fit(
+        train_dataset,
+        valid_dataset=eval_dataset,
+        train_collate_fn=DataCollatorForTokenClassification(tokenizer),
+        valid_collate_fn=DataCollatorForTokenClassification(tokenizer),
+        callbacks=[es],
+        config=config,
+    )
+model.model.load_state_dict(torch.load(os.path.join(output_dir, f"fold{cfg.fold}.pt")))
+# score, oof_df = eval_token_cls_model(model.model, [s for s in samples if s['fold'] == cfg.fold], pooling=cfg.pooling)
+# oof_df = convert_oof(oof_df)
+# oof_df.to_csv(os.path.join(output_dir, f"fold{cfg.fold}_oof.csv"), index=False)
+# print(f"fold {cfg.fold}: score={score}")
+# save_json({**vars(cfg), 'score': score, 'seed_used': seed}, os.path.join(output_dir, f"fold{cfg.fold}.json"))
+# get_cv(output_dir)
+# get_oof(output_dir)
