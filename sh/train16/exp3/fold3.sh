c6f0db08d4b3085c5f1fd6badf10326542f75f81
Zacchaeus14 <yw3642@nyu.edu>
2022-08-16 21:11:58 +0800

    add exps

---
+++ sh/train16/exp3/fold3.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH --job-name=train16-exp3-fold3
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=24GB
+#SBATCH --time=167:00:00
+#SBATCH --mail-type=END
+#SBATCH --mail-user=yw3642@nyu.edu
+#SBATCH --output=log/%x-%A.out
+#SBATCH --error=log/%x-%A.err
+#SBATCH --gres=gpu:1
+#SBATCH -p aquila
+#SBATCH --nodelist=agpu8
+
+module purge
+module load anaconda3 cuda/11.1.1
+
+nvidia-smi
+nvcc --version
+cd /gpfsnyu/scratch/yw3642/fb2/src
+
+echo "START"
+source deactivate
+source /gpfsnyu/packages/anaconda3/5.2.0/bin/activate kaggle
+python -u train16.py --ckpt /gpfsnyu/scratch/yw3642/hf-models/google_bigbird-roberta-large \
+--epochs 10 --batch_size 2 --lr 5e-6 --lr_head 1e-4 --seed -1 --fold 3 --exp 3 \
+--use_pretrained /gpfsnyu/scratch/yw3642/fb2/ckpt/pretrain1/exp2/google_bigbird-roberta-large/checkpoint-116970 \
+--adv_lr 1 --adv_eps 0.00025 --adv_after_epoch 1 --patience 30 --gradient_checkpointing \
+--warmup_ratio 0.1 --resize_embedding
+
+echo "FINISH"
